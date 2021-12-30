d0f0aa493b126680ede60e1713e369b597d53b21
julian776 <juliancamiloalvarez77@gmail.com>
2021-12-30 15:51:53 -0500

    Proplema arreglado

--- src/config/store.js
+++ src/config/store.js
@@ -1,9 +1,14 @@
-
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers,applyMiddleware, compose } from "redux";
 
 import * as reducers from '../reducers'
 
+import thunk from "redux-thunk";
+
 const rootReducer = combineReducers(reducers)
 
+const composeEnhacer =  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
-export const store = createStore(rootReducer)
+export const store = createStore(
+  rootReducer,
+  composeEnhacer(applyMiddleware(thunk))
+);
